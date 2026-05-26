11d7e79491f6a8f6ebc771f31e534e3cb1739d62
Bambu <byrr113@gmail.com>
2026-05-27 00:17:05 +0900

    ✨feat: UILabel 확장, 주석 수정
    - LabelConfiguration 활용 생성자
    - 기존 label attirbutes 변경 편의 메서드

--- WhereToFit/WhereToFit/Util/LabelConfiguration.swift
+++ WhereToFit/WhereToFit/Util/LabelConfiguration.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 
+// dyanamic text style 우선 미적용으로 두었고, 추후 적용 가능성이 있기에 기존 코드는 주석처리 해두었습니다.
 struct LabelConfiguration {
     let font: UIFont
 //    let textStyle: UIFont.TextStyle
@@ -15,17 +16,15 @@ struct LabelConfiguration {
 }
 
 /*
- 현재는 정해진 폰트 굵기와 크기 외에 변경은 막아두지 않았습니다.
- 
  생성으로 사용시
- let titleLabel = UILabel(text: "제목", config: .headline24)
+ let titleLabel = UILabel(text: "제목", config: .title24)
  let bodyLabel = UILabel(text: "본문", config: .body16)
 
  이미 있는 label 적용
- label.apply(.body16)
- label.apply(.body16, color: .secondaryLabel)
- label.apply(.body16, lines: 0)
- label.apply(.body16, color: .systemRed, lines: 2)
+ label.apply(.systemFont(ofSize: 16, weight: .regular))
+ label.apply(.systemFont(ofSize: 16, weight: .regular), color: .secondaryLabel)
+ label.apply(lines: 0)
+ label.apply(color: .systemRed, lines: 2)
  */
 
 extension LabelConfiguration {
