247fac05a66c69c341b1c86cce04e4e432f355fa
Bambu <byrr113@gmail.com>
2026-05-28 01:17:19 +0900

    ✨feat: TitleView 생성
    - TempViewController에 구현 예시 추가

--- WhereToFit/WhereToFit/View/TempViewController.swift
+++ WhereToFit/WhereToFit/View/TempViewController.swift
@@ -9,9 +9,28 @@ import UIKit
 import ReactorKit
 
 final class TempViewController: BaseViewController<TempReactor> {
+    let titleView = TitleView(text: "위치 지정", leftButtonImage: .close)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        view.addSubview(titleView)
+        
+        titleView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            titleView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            titleView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            titleView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+    }
+    
+    override func bind(reactor: TempReactor) {
+        titleView.rx.leftButtonTap
+            .subscribe(onNext: {
+                print("leftButtonTapped")
+            })
+            .disposed(by: disposeBag)
     }
 }
 
