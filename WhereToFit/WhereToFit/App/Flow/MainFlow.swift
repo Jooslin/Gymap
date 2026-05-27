f0dfbbd3195a83183cd3c6a34cea0d62e47a74b5
Bambu <byrr113@gmail.com>
2026-05-27 15:27:57 +0900

    ✨feat: MainFlow 생성

---
+++ WhereToFit/WhereToFit/App/Flow/MainFlow.swift
@@ -0,0 +1,105 @@
+//
+//  MainFlow.swift
+//  WhereToFit
+//
+//  Created by 변예린 on 5/27/26.
+//
+
+import UIKit
+import RxFlow
+import ReactorKit
+
+final class MainFlow: Flow {
+    private let window: UIWindow
+    private let tabBarController = UITabBarController()
+    
+    var root: any Presentable { tabBarController }
+    
+    init(window: UIWindow) {
+        window.rootViewController = tabBarController
+        self.window = window
+    }
+    
+    func navigate(to step: any RxFlow.Step) -> FlowContributors {
+        guard let step = step as? AppStep else {
+            return .none
+        }
+        
+        switch step {
+        case .main:
+            return navigateToMain()
+            
+        default:
+            return .one(flowContributor: .forwardToParentFlow(withStep: step))
+        }
+    }
+    
+    private func navigate(to viewController: UIViewController, animated: Bool) {
+        if let navigationController = tabBarController.selectedViewController as? UINavigationController {
+            navigationController.pushViewController(viewController, animated: animated)
+        } else {
+            tabBarController.selectedViewController?.present(viewController, animated: animated, completion: nil)
+        }
+    }
+    
+    private func present(_ viewController: UIViewController, animated: Bool) {
+        tabBarController.selectedViewController?.present(viewController, animated: animated, completion: nil)
+    }
+    
+    private func pop(animated: Bool) {
+        if let navigationController = tabBarController.selectedViewController as? UINavigationController {
+            navigationController.popViewController(animated: animated)
+        } else {
+            tabBarController.selectedViewController?.dismiss(animated: animated)
+        }
+    }
+}
+
+extension MainFlow {
+    private func navigateToMain() -> FlowContributors {
+        
+        let plantTabFlow = PlantTabFlow()
+        let calendarTabFlow = CalendarTabFlow()
+        let myInfoTabFlow = MyInfoTabFlow()
+        
+        // Flow를 준비 - 클로저는 Flow가 배치될 준비가 되었을 때(Flow의 첫 번째 화면이 선택되었을 때) 실행될 동작
+        // Flow.use는 내부에서 Single 이벤트를 drive로 구독을 소비하므로 소비 완료 후 자동으로 구독이 해제되어 메모리 누수가 발생하지 않음
+        Flows.use(plantTabFlow, calendarTabFlow, myInfoTabFlow, when: .created) { plant, calendar, my in
+            
+            plant.tabBarItem = UITabBarItem(
+                title: "홈",
+                image: .houseEmpty,
+                selectedImage: .houseFill
+            )
+            
+            calendar.tabBarItem = UITabBarItem(
+                title: "캘린더",
+                image: .calendarEmpty,
+                selectedImage: .calendarFill,
+            )
+            
+            my.tabBarItem = UITabBarItem(
+                title: "마이",
+                image: .userEmpty,
+                selectedImage: .userFill
+            )
+            
+            self.tabBarController.setViewControllers([plant, calendar, my], animated: true)
+            self.tabBarController.tabBar.tintColor = .primary700
+        }
+        
+        return .multiple(flowContributors: [
+            .contribute(withNextPresentable: plantTabFlow, withNextStepper: OneStepper(withSingleStep: AppStep.plantTab)),
+            .contribute(withNextPresentable: calendarTabFlow, withNextStepper: OneStepper(withSingleStep: AppStep.calendarTab)),
+            .contribute(withNextPresentable: myInfoTabFlow, withNextStepper: OneStepper(withSingleStep: AppStep.myInfoTab))
+        ])
+    }
+    
+    private func presentAlert(title: String, message: String) -> FlowContributors {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "확인", style: .default))
+        
+        present(alert, animated: true)
+        return .none
+    }
+}
