0e44c1bc48366138f0bf6c6baaa750923cc7a07f
Bambu <byrr113@gmail.com>
2026-05-30 17:19:00 +0900

    ✨feat: HomeWeatherCell Reactive 확장
    - 프로그램 등록 버튼, 운동 기록 버튼 Reactive 확장

--- WhereToFit/WhereToFit/View/Home/Cell/HomeWeatherCell.swift
+++ WhereToFit/WhereToFit/View/Home/Cell/HomeWeatherCell.swift
@@ -8,8 +8,12 @@
 import UIKit
 import SnapKit
 import Then
+import RxSwift
+import RxCocoa
 
 final class HomeWeatherCell: UICollectionViewCell {
+    private(set) var disposeBag = DisposeBag()
+    
     private lazy var weeklyDateView = generateWeeklyDateView()
     private let weatherImageView = UIImageView()
     private let weatherLabel = UILabel(config: .body14Medium)
@@ -29,6 +33,11 @@ final class HomeWeatherCell: UICollectionViewCell {
     @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        disposeBag = DisposeBag()
     }
 }
 
@@ -88,4 +97,14 @@ extension HomeWeatherCell {
         }
     }
 
+}
+
+extension Reactive where Base: HomeWeatherCell {
+    var registerButtonTap: ControlEvent<Void> {
+        base.registrationButton.rx.tap
+    }
+    
+    var recordButtonTap: ControlEvent<Void> {
+        base.recordButton.rx.tap
+    }
 }
