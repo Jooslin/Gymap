eb51348be1d6ef795b1baa5c94f0bcba059de748
Bambu <byrr113@gmail.com>
2026-05-30 15:13:12 +0900

    ✨feat: API 열거형 생성

--- WhereToFit/WhereToFit/Service/NetworkService.swift
+++ WhereToFit/WhereToFit/Service/NetworkService.swift
@@ -11,3 +11,19 @@ import Alamofire
 final class NetworkService {
     
 }
+
+extension NetworkService {
+    enum API {
+        case weather
+        case facility
+        
+        var basrUrl: String {
+            switch self {
+            case .weather:
+                "https://api.openweathermap.org/data/2.5/weather"
+            case .facility:
+                
+            }
+        }
+    }
+}
