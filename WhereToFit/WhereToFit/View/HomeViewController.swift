cdbb5aee23ab1173e50c0e3c5145dce4ece0f8f3
Bambu <byrr113@gmail.com>
2026-05-30 15:27:47 +0900

    🚚build: HomeTab 기본 사항 설정
    - background image asset 추가
    - HomeVC, HomeReactor 생성
    - HomeFlow vc 수정

---
+++ WhereToFit/WhereToFit/View/HomeViewController.swift
@@ -0,0 +1,12 @@
+//
+//  HomeViewController.swift
+//  WhereToFit
+//
+//  Created by 변예린 on 5/30/26.
+//
+
+import UIKit
+
+final class HomeViewController: BaseViewController<HomeReactor> {
+    
+}
